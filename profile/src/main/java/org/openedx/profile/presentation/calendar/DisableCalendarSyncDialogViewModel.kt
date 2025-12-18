a0c0c928943f74187d2aeda45e110f7117d8e20b
PavloNetrebchuk <141041606+PavloNetrebchuk@users.noreply.github.com>
2025-12-18 13:55:58 +0200

    feat: Google calendar sync (#470)
    * feat: google calendar sync
    
    * fix: deleting not courses events
    
    * fix: switching between local and google calendars. Google calendars options.

--- profile/src/main/java/org/openedx/profile/presentation/calendar/DisableCalendarSyncDialogViewModel.kt
+++ profile/src/main/java/org/openedx/profile/presentation/calendar/DisableCalendarSyncDialogViewModel.kt
@@ -1,9 +1,15 @@
 package org.openedx.profile.presentation.calendar
 
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.openedx.core.data.storage.CalendarPreferences
 import org.openedx.core.domain.interactor.CalendarInteractor
+import org.openedx.core.domain.model.CalendarType
 import org.openedx.core.system.CalendarManager
 import org.openedx.core.system.notifier.calendar.CalendarNotifier
 import org.openedx.core.system.notifier.calendar.CalendarSyncDisabled
@@ -16,12 +22,35 @@ class DisableCalendarSyncDialogViewModel(
     private val calendarInteractor: CalendarInteractor,
 ) : BaseViewModel() {
 
+    private val _deletionState = MutableStateFlow<DeletionState?>(null)
+    val deletionState: StateFlow<DeletionState?> = _deletionState.asStateFlow()
+
     fun disableSyncingClick() {
         viewModelScope.launch {
-            calendarInteractor.clearCalendarCachedData()
-            calendarManager.deleteCalendar(calendarPreferences.calendarId)
-            calendarPreferences.clearCalendarPreferences()
-            calendarNotifier.send(CalendarSyncDisabled)
+            try {
+                withContext(NonCancellable) {
+                    _deletionState.value = DeletionState.DELETING
+                    val allEvents = calendarInteractor.getAllCourseCalendarEventsFromCache()
+                    val eventIds = allEvents.map { it.eventId }
+                    calendarManager.deleteEvents(eventIds)
+                    _deletionState.value = DeletionState.DELETED
+                    calendarInteractor.clearCalendarCachedData()
+                    val calendarId = calendarPreferences.calendarId
+                    if (calendarPreferences.calendarType == CalendarType.LOCAL) {
+                        calendarManager.deleteCalendar(calendarId)
+                    }
+                    calendarPreferences.clearCalendarPreferences()
+                    calendarNotifier.send(CalendarSyncDisabled)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                _deletionState.value = null
+            }
         }
     }
+}
+
+enum class DeletionState {
+    DELETING, DELETED
 }

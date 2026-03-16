0cb5270f78f32d9fc74d57aaa1989fe49c0bb260
Your Name <you@example.com>
2026-03-16 17:15:13 +0530

    logo set center

--- core/src/main/java/org/openedx/core/ui/theme/compose/LogistrationLogoView.kt
+++ core/src/main/java/org/openedx/core/ui/theme/compose/LogistrationLogoView.kt
@@ -1,12 +1,15 @@
 package org.openedx.core.ui.theme.compose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -18,14 +21,19 @@ import org.openedx.core.ui.theme.appColors
 
 @Composable
 fun LogistrationLogoView() {
-    Image(
-        modifier = Modifier
-            .padding(top = 64.dp, bottom = 20.dp)
-            .width(120.dp)
-            .height(80.dp),
-        painter = painterResource(id = R.drawable.logo_white_bg),
-        contentDescription = null
-    )
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(top = 64.dp, bottom = 20.dp)
+                .width(120.dp)
+                .height(80.dp),
+            painter = painterResource(id = R.drawable.logo_white_bg),
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(widthDp = 375)
